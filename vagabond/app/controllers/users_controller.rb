0b3b883fd2736fec688b99c37df73a64179c6fc5
billhimmelsbach <whimmels@gmail.com>
2016-08-18 14:28:11 -0700

    adds bcrypt to the server

--- vagabond/app/controllers/users_controller.rb
+++ vagabond/app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def splash
     render :splash
   end
@@ -26,6 +27,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password_digest)
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
   end
 end
