be2e474f54aff231a8dbd9e0172f3f93055a49fe
danieloh91 <daniel.oh.91@gmail.com>
2016-08-18 16:58:03 -0700

    adds current_city into user_params to show on update

--- vagabond/app/controllers/users_controller.rb
+++ vagabond/app/controllers/users_controller.rb
@@ -41,6 +41,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :current_city)
   end
 end
